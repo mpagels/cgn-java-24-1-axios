050e4b3196cf3b6c4f3c42f57bf5070a1c9fb835
mpagels <54703643+mpagels@users.noreply.github.com>
2024-04-19 15:23:42 +0200

    app can post data to a resource url

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
-
+import "./App.css"
 
 type Person = {
     id: number,
@@ -14,15 +14,21 @@ type Person = {
 function App() {
 
     const [data, setData] = useState<Person[]>([]);
+    const [page, setPage] = useState<number>(1)
+    const [name, setName] = useState<string>("")
+    const [job, setJob] = useState<string>("")
 
+    function updatePage(number: number) {
+        setPage(number)
+    }
 
     useEffect(() => {
-        fetchData()
-    }, [])
+        fetchData(page)
+    }, [page])
 
 
-    function fetchData() {
-        axios.get("https://reqres.in/api/users").then((response) => {
+    function fetchData(page: number) {
+        axios.get("https://reqres.in/api/users?page=" + page).then((response) => {
             setData(response.data.data)
         }).catch(error => {
             console.log(error.message)
@@ -31,6 +37,10 @@ function App() {
 
     console.log(data)
 
+    function postData() {
+        axios.post("https://reqres.in/api/users", { name: name, job: job}).then(response => console.log(response))
+    }
+
   return (
     <>
         {data.length === 0 ? <h1>NO DATA</h1>: <ul>
@@ -38,6 +48,13 @@ function App() {
                 return <li key={person.id}>{person.first_name} {person.last_name}</li>
             })}
         </ul>}
+
+        <ul className={"pagination"}><li className={"item"} onClick={() => updatePage(1)}> 1</li><li className={"item"} onClick={() => updatePage(2)}>2</li></ul>
+        <label>Name</label>
+        <input type={"text"} onChange={(event) => setName(event.target.value)} value={name}/>
+        <label>Job</label>
+        <input type={"text"} onChange={(event) => setJob(event.target.value)} value={job}/>
+        <button onClick={postData}>Abschicken</button>
 
     </>
   )
